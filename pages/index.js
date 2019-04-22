98dc96324c2ed06ac96ddae0090e7c7c46a8499c
Harikrishnan <hkkrishn563@gmail.com>
2019-04-22 16:54:21 -0700

    setup

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
-const Index = () =>{
-  return(
-    <h1>Index Page</h1>
-  )
+import React, { Component } from 'react'
+
+//class components get and receive data
+//class components can use lifecycle functions etc and have more functionality
+//render method is hallmark of class compnents
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        Class Component Index Page!
+      </div>
+    )
+  }
 }
 
-export default Index;
+export default Index
